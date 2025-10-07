b85da2dd1e6612c135eab35b9bcebcec418b3d86
SevenGriphon <152780844+SeveneGriphon@users.noreply.github.com>
2025-10-07 17:08:28 +0200

    Fix issues with hitboxes
    Added missing script to hitbox, added print to hitbox script for debugging and fixed error with missing argument in hurtbox script

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -3,4 +3,5 @@ class_name Hitbox extends Area2D
 signal damaged(damage)
 
 func take_damage(damage):
+	print(damage)
 	damaged.emit(damage)
--- scripts/hurtbox.gd
+++ scripts/hurtbox.gd
@@ -4,4 +4,4 @@ extends Area2D
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is Hitbox:
-		area.take_damage()
+		area.take_damage(damage)
