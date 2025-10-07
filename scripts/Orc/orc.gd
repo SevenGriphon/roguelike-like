9372843e846ec4de5a58cb6d89aecce549758d9d
SevenGriphon <152780844+SeveneGriphon@users.noreply.github.com>
2025-10-07 20:35:51 +0200

    Add hitbox to orc

--- scripts/Orc/orc.gd
+++ scripts/Orc/orc.gd
@@ -1,10 +1,20 @@
 extends CharacterBody2D
 
 @onready var anim_sprite :AnimatedSprite2D = $AnimatedSprite2D
+@export var hitbox : Hitbox
+
 var health = 100
 var knockback = 100
 signal damaged
 signal died
+
+func _ready() -> void:
+	hitbox.damaged.connect(func(damage):
+		health -= damage
+		damaged.emit()
+		#if health > 0:
+			#self.velocity = knockback_direction.normalized() * knockback
+	)
 
 func _process(delta: float) -> void:
 	if self.velocity.x < 0:
@@ -14,9 +24,3 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
-
-func damage(amount, knockback_direction : Vector2):
-	health -= amount
-	damaged.emit()
-	if health > 0:
-		self.velocity = knockback_direction.normalized() * knockback
