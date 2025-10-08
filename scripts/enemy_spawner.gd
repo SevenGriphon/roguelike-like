a014b74b02b8a250b8d5e63cb648e888e6468449
SevenGriphon <152780844+SeveneGriphon@users.noreply.github.com>
2025-10-08 22:56:38 +0200

    Modify and enable enemy spawning system

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -4,42 +4,60 @@ extends Node
 @export var marker_tscn :PackedScene
 @export var min_spawn_time = 10
 @export var max_spawn_time = 30
+@export var tilemap : TileMapLayer
+
 var wave = 0
 var alive_enemies : int
-
-func start_new_wave() -> void:
-	return
-	wave += 1
-	for enemy in range(wave*3):
-		prepare_to_spawn_enemy()
-
-func prepare_to_spawn_enemy():
-	var marker :Node2D = marker_tscn.instantiate()
-	get_tree().current_scene.add_child.call_deferred(marker)
-	
-	var x_pos = randf_range(0, get_viewport().size.x)
-	var y_pos = randf_range(0, get_viewport().size.y)
-	var position = Vector2(x_pos, y_pos)
-	marker.position = position
-	
-	var delay_timer :Timer = marker.get_node("Timer")
-	delay_timer.connect("timeout", func(): # connecting enemy spawning to timer timeout
-		spawn_enemy(position, marker)
-	)
-
-func spawn_enemy(position, marker :Node2D):
-	marker.queue_free()
-	
-	var orc :CharacterBody2D = orc_tscn.instantiate()
-	orc.position = position
-	get_tree().current_scene.add_child(orc)
-	
-	orc.died.connect(func():
-		alive_enemies -= 1
-		if alive_enemies <= 0:
-			start_new_wave()
-	)
-	alive_enemies += 1
+var wave_enemies : Array = []
 
 func _ready() -> void:
 	start_new_wave()
+
+func start_new_wave() -> void:
+	#return
+	wave += 1
+	var valid_positions = get_valid_spawn_positions()
+	queue_enemies(wave*3, valid_positions)
+	await get_tree().create_timer(1).timeout
+	spawn_enemies()
+
+func get_valid_spawn_positions() -> Array:
+	var valid_pos = []
+	for pos in tilemap.get_used_cells():
+		var tile_data = tilemap.get_cell_tile_data(pos)
+		if tile_data.get_custom_data("can_spawn"):
+			var local_pos = tilemap.map_to_local(pos)
+			valid_pos.append(tilemap.to_global(local_pos))
+	return valid_pos
+
+
+func queue_enemies(amount : int, positions : Array):
+	wave_enemies = []
+	for i in range(amount):
+		var enemy_pos = positions[randi_range(0, len(positions))]
+		
+		var marker :Node2D = marker_tscn.instantiate()
+		get_tree().current_scene.add_child.call_deferred(marker)
+		
+		marker.position = enemy_pos
+		var enemy_info : Dictionary = {
+			"position": enemy_pos,
+			"marker": marker,
+		}
+		
+		wave_enemies.append(enemy_info)
+
+func spawn_enemies():
+	for enemy_info in wave_enemies:
+		enemy_info.marker.queue_free()
+		
+		var orc :CharacterBody2D = orc_tscn.instantiate()
+		orc.position = enemy_info.position
+		get_tree().current_scene.add_child(orc)
+		
+		orc.died.connect(func():
+			alive_enemies -= 1
+			if alive_enemies <= 0:
+				start_new_wave()
+		)
+		alive_enemies += 1
